ed2bb1abb7337136841a7233c56c272492194dc3
bharadwajwise <bharadwaj.s@wisecells.com>
2013-10-01 16:26:57 +0530

    added other API calls left out in initial commit

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 ideone_python_example
 =====================
 
-An example script that has ready to use code for accessing ideone API using Python.
+The example.py is an example script that has ready to use code for accessing ideone API using Python.
 
-If you want to use a wrapper that's more easier to use, use ideone-api. 
+It took only couple of hours for me to explore this API. But, I couldn't find any good resources or practical examples for using this API (in Python). 
 
-https://github.com/jschaf/ideone-api
+So, I've compiled all the basic API calls into this example repo, to ease the process of developing for people who might be searching for ideone python examples in the future.
 
-Some snippets of code in this repo have been taken from the above mentioned repo. Please check it out.
+If you want to use a wrapper over this API, use ideone-api: https://github.com/jschaf/ideone-api
+
+I have taken many references from many places on the Internet while writing this repo. The above repository helped me the most.
+
+Make sure to keep the ideone pdf documentation along side this when you write your code for the first time: http://ideone.com/files/ideone-api.pdf
+
+Above all, enjoy!
--- example.py
+++ example.py
@@ -1,8 +1,11 @@
 # Read comments to understand what the following snippet does.
-
-# these are used in almost every API call we make to ideone.
+# You're free to use these snippets however you like. 
+#
+# Last tested on 1st October 2013.
+#
 # API password is different from Account password. 
 # Checkout ideone settings for your account to setup one.
+# They're used in every ideone API call.
 USER = "username"
 PASS = "password"
 
@@ -24,9 +27,14 @@ local_server = SOAP.SOAPServer(("localhost", 8080))
 s=local_server.registerFunction(hello)
 local_server.serve_forever()
 
+###########################################################
+
 # Creating WSDLObject => First step.
 from SOAPpy import WSDL
 wsdlObject = WSDL.Proxy("http://ideone.com/api/1/service.wsdl")
+
+# test function to test your connection/ USER-PASS combination.
+wsdlObject.testFunction(USER, PASS)
 
 ###########################################################
 
@@ -54,3 +62,52 @@ def get_language_id(LANGUAGE_NAME):
 
 ###########################################################
 
+# A submission example. Submitting a file as my sourceCode.
+# We could have also used a sample string like this instead:
+# str1 = "print 'Hello World'"
+
+f = open('hello.py', 'r')
+str1 = f.read()
+f.close()
+
+# initializing all the arguments to be passed. Change them like you want.
+sourceCode = str1
+language = get_language_id('Python') # could have used the id too.
+input = ""
+run = True
+private = True
+
+# We need this object for accessing the link of this submission, which will be used for fetching results.
+submission_object = wsdlObject.createSubmission(USER, PASS, sourceCode, language, input, run, private)
+
+link = list(submission_object)[0][1]['value']
+
+# To get status of the submission.
+submission_status = wsdlObject.getSubmissionStatus(USER, PASS, link)
+
+###########################################################
+
+# choose your own values here.
+withSource = True
+withInput = True
+withOutput = True
+withStderr = True
+withCmpinfo = True
+
+# To get details of the submission.
+submission_details = wsdlObject.getSubmissionDetails(USER, PASS, link, withSource, withInput, withOutput, withStderr, withCmpinfo)
+
+# this is how you would read each detail of the submission, similar to one we did before.
+#
+# Convert the details result into something more approachable, because 
+# this one could contain many key, value pairs depending on the parameters we might have passed. 
+submission_details_list = list(submission_details)[0]
+submission_details_dict = {}
+for i in range(0, len(submission_details_list)):
+	submission_details_dict[submission_details_list[i]['key']] = submission_details_list[i]['value']
+
+# To access one of the values in the big details object created...
+# for example result of the submission - you now have to just do this:
+submission_details_dict['result']
+
+###########################################################
